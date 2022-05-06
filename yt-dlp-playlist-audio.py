5b1b8617d9940946384063e8466f4b918c4669e6
wukinnin <84181231+wukinnin@users.noreply.github.com>
2022-05-06 13:48:57 +0800

    initial commit

---
+++ yt-dlp-playlist-audio.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+import os
+
+# It is required that yt-dlp and ffmpeg is installed on your system.
+
+url = input('Enter URL: ')
+
+# Output can be changed
+
+os.system('yt-dlp -x --audio-format m4a -o "%(playlist_index)s.%(title)s.%(ext)s" {}'.format(url))
---
+++ yt-dlp-playlist-video.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+import os
+
+# It is required that yt-dlp and ffmpeg is installed on your system.
+
+url = input('Enter URL: ')
+
+# Output can be changed
+
+os.system('yt-dlp -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4 -o "%(playlist_index)s.%(title)s.%(ext)s" {}'.format(url))
